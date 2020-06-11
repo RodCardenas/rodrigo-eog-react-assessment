7ffa444ac67022edf68afb08f22fe2b4cdbbbf37
Rodrigo Cardenas <rcardenas@utexas.edu>
2020-06-11 08:03:18 -0500

    Replace MUI select with react select to facilitate clearable selected elements.

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import FormControl from '@material-ui/core/FormControl';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
+import Select, { ValueType, OptionTypeBase } from 'react-select';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
@@ -22,28 +18,23 @@ type SelectProps = {
 
 export default ({ options, label, onChosenChange }: SelectProps) => {
   const classes = useStyles();
-  const [chosen, setChosen] = React.useState([]);
+  const [chosen, setChosen] = React.useState<ValueType<OptionTypeBase>>([]);
 
-  // TODO: event: React.ChangeEvent<HTMLInputSelect> not supported by MUI https://github.com/mui-org/material-ui/issues/15400
-  const handleChange = (event: any) => {
-    setChosen(event.target.value);
-    if (onChosenChange) {
-      onChosenChange(event.target.value);
+  const handleChange = (selected: any) => {
+    if (selected) {
+      setChosen(selected);
+      if (onChosenChange) {
+        const newChosenOptions = selected.map((option: any) => {
+          return option.label;
+        });
+        onChosenChange(newChosenOptions);
+      }
     }
   };
 
-  return (
-    <FormControl className={classes.select}>
-      <InputLabel id={label}>{label}</InputLabel>
-      <Select labelId={label} id={label + 'select'} multiple value={chosen} onChange={handleChange} input={<Input />}>
-        {options.map(option => {
-          return (
-            <MenuItem key={option} value={option}>
-              {option}
-            </MenuItem>
-          );
-        })}
-      </Select>
-    </FormControl>
-  );
+  const selectOptions = options.map(option => {
+    return { label: option, value: option };
+  });
+
+  return <Select className={classes.select} isMulti value={chosen} onChange={handleChange} options={selectOptions} />;
 };
